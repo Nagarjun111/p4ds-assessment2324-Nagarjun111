6c93f820e92bb3a54a04358682ee6b7ca80051e5
Nagarjun111 <153112527+Nagarjun111@users.noreply.github.com>
2024-01-25 13:22:30 +0500

    Assignment Uploaded

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    # border_char = '|'
+    # for tuple in coefficients_list:
+    #     print(f"{tuple[0]:<15}{'   ' + border_char + '   ' }{tuple[1]:<15}{'   ' + border_char + '   ' }{tuple[2]:<15}")
